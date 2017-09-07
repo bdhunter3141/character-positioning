dc1633f6194a51a2881efb1e5ddfe7d1424e1e42
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-07 19:17:16 +0000

    added functionality to add letters to object with no repeats

--- character-positioning.js
+++ character-positioning.js
@@ -1,18 +1,65 @@
-const countLetters = function(str) {
-  const lettersObj = {}
-  let splitStr = str.split("")
-  let sortStr = splitStr.sort().join("")
-  //Create loop to cycle through splitStr and add letter to lettersObj
-  for (let i = 0; i < sortStr.length; i++) {
-    if (sortStr[i] !== sortStr[i+1]) {
-      const theLetter = sortStr[i]
-      const theNumber = str.split(sortStr[i]).length - 1
-      lettersObj[theLetter] = theNumber
+// return all the indices (zero-based positions) in the string where each character is found.
+// So for each letter, we're no longer returning just one number to represent its number of occurrences,
+// but rather (potentially) multiple numbers to represent all the places in the string that it shows up.
+
+
+
+
+const indexFinder = function(str) {
+  const newObject = {}
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  //values being an array of their indices
+  for (var i = 0; i < alphabet.length; i++) {
+    if (str.includes(alphabet[i])) {
+      newObject[alphabet[i]] = ""
     }
   }
-  return lettersObj
+  //values being an array of their indices
+
+  return newObject
 }
 
-console.log(countLetters("alsjfilewajklvhkagldjskflash"))
 
 
+
+console.log(indexFinder("lighthouse in the house"))
+
+
+
+
+
+
+
+// create array of individual letters
+// use array to loop through old array and find index
+
+
+
+// const countLetters = function(str) {
+//   const lettersObj = {}
+//   for (let letter of str) {
+//     if (str[letter]){
+//   }
+//   return lettersObj
+// }
+
+// console.log(countLetters("alsjfilewajklvhkagldjskflash"))
+
+
+// const findLetters = function(str) {
+//   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//   let letterList = {}
+//   for (let x = 0; x < alphabet.length; x++) {
+//     for (let i = 0; i < str.length; i++) {
+//       // console.log(str[i], alphabet[x])
+//       if (str[i] === alphabet[x]) {
+//         const pushLetter = str[i]
+//         console.log(pushLetter)
+//         letterList[pushLetter]
+//       }
+//     }
+//   }
+//   return letterList
+// }
+
+// console.log(findLetters("aksldhgksdaljfa"))
